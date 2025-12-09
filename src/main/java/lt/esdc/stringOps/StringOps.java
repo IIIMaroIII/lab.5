3f309195eee79495f8132ec38ea9f11ae6824974
rod <matveev.r.v@icloud.com>
2025-12-10 01:45:24 +0200

    quick_commit

--- src/main/java/lt/esdc/stringOps/StringOps.java
+++ src/main/java/lt/esdc/stringOps/StringOps.java
@@ -1,21 +1,37 @@
 package lt.esdc.stringOps;
 
-import net.datafaker.Faker;
-
-import java.util.List;
+import java.util.Arrays;
 
 public class StringOps {
     public static void main(String[] args) {
-    new StringOps().init();
+        new StringOps().init();
     }
 
-     public void init() {
-         determineRepeatableWords();
+    public void init() {
+        determineRepeatableWords();
     }
 
-public void determineRepeatableWords() {
-        String input = "Sun shines bright sun SHINES rain falls Rain falls wind gentle WIND gentle ocean blue Ocean blue Earth";
+    public void determineRepeatableWords() {
+        String input = "Sun shines bright sun sun SHINES rain falls Rain falls wind gentle WIND gentle ocean blue Ocean blue Earth".toLowerCase();
+
         String[] arr = input.split("\s+");
-    System.out.println(arr.toString());
-}
+
+        Arrays.sort(arr);
+        System.out.println("arr.length: " + arr.length);
+        System.out.println("arr: " + Arrays.toString(arr));
+        int counter = 1;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (i < arr.length - 1 && arr[i].equalsIgnoreCase(arr[i + 1])) {
+//                System.out.format("Occured %s vs %s\n", arr[i], arr[i + 1]);
+//                System.out.println(arr[i]);
+                counter++;
+                if (counter % 2 != 0) {
+                    System.out.println(arr[i]);
+                }
+            }
+
+        }
+
+    }
 }
